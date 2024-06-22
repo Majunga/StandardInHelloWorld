36cad74871cffaeea72a510890892e50489b068e
Ethan McWilliams <ethan.mcwilliams@majunga.co.uk>
2024-06-22 14:27:51 +0100

    Add project files.

---
+++ StandardInHelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿string? s;
+Console.WriteLine("Enter text. Enter a blank line to exit.");
+while ((s = Console.ReadLine()) is not null)
+{
+    if (string.IsNullOrWhiteSpace(s))
+    {
+        break;
+    }
+
+    Console.WriteLine(s);
+}
